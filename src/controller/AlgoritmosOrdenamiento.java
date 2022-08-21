ac02dc503e470d32758b96a98e98a38c0340fe0e
JJeveloper <jaroodjimili@gmail.com>
2022-08-20 19:03:44 -0500

    algoritmos de ordenamiento

--- src/controller/AlgoritmosOrdenamiento.java
+++ src/controller/AlgoritmosOrdenamiento.java
@@ -29,4 +29,55 @@ public class AlgoritmosOrdenamiento {
         return numeros;
     }
 
+    public int[] burbuja() {
+        int aux;
+        
+        for (int i = 0; i < numeros.length - 1; i++) {
+
+            for (int j = 0; j < numeros.length - 1; j++) {
+                if (numeros[j] > numeros[j + 1]) {
+                    aux = numeros[j + 1];
+                    numeros[j + 1] = numeros[j];
+                    numeros[j] = aux;
+                }
+            }
+        }
+        return numeros;
+    }
+
+    public int[] seleccion() {
+        int aux, menor;
+
+        
+        for (int i = 0; i < numeros.length - 1; i++) {
+            menor = i;
+            for (int j = i + 1; j < numeros.length; j++) {
+                if (numeros[j] < numeros[menor]) {
+                    menor = j;
+                }
+            }
+            aux = numeros[i];
+            numeros[i] = numeros[menor];
+            numeros[menor] = aux;
+        }
+
+        return numeros;
+    }
+
+    public int[] insercion() {        
+        int aux, posicion;
+        
+        for (int i = 0; i < numeros.length; i++) {
+            posicion = i;
+            aux = numeros[i];
+
+            while ((posicion > 0) && (numeros[posicion - 1] > aux)) {
+                numeros[posicion] = numeros[posicion - 1];
+                posicion--;
+            }
+            numeros[posicion] = aux;
+        }
+        return numeros;
+    }
+
 }
